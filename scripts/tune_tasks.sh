b32b5c5becb5336b42ab6c6a6cc0d33a4864dbf8
Silvio Moreira <silvio.moreira@qntfy.com>
2020-11-03 09:34:40 -0800

    added fairness tuning

--- scripts/tune_tasks.sh
+++ scripts/tune_tasks.sh
@@ -1,15 +1,15 @@
-BASE_PATH="/Users/samir/Dev/projects/MIMIC/MIMIC"
-# BASE_PATH="/media/data_1/samir/projects/MIMIC"
+# BASE_PATH="/Users/samir/Dev/projects/MIMIC/MIMIC"
+BASE_PATH="/media/data_1/samir/projects/MIMIC"
 INPUT=$BASE_PATH"/DATA/input/"
 FEATURES=$BASE_PATH"/DATA/features/"
-OUTPUT=$BASE_PATH"/DATA/results_sensitivity/"
-CACHE=$BASE_PATH"/DATA/processed_sensitivity/"
+OUTPUT=$BASE_PATH"/DATA/results_auroc/"
+CACHE=$BASE_PATH"/DATA/processed_auroc/"
 DATA="tasks"
 MODEL="BERT-POOL"
 
 python src/main.py -input_path $INPUT -dataset $DATA -model $MODEL -feats_path $FEATURES \
                    -output_path $OUTPUT \
-                   -cache_path $CACHE -mini_tasks -tune "sensitivity"
+                   -cache_path $CACHE -tune "auroc" -fairness
                    
                     
 
--- src/main.py
+++ src/main.py
@@ -13,8 +13,8 @@ def get_args():
     par.add_argument('-tune', type=str, help='tuning metric')
     par.add_argument('-mini_tasks', action="store_true", help='use small datasets')
     par.add_argument('-reset_tasks', action="store_true", help='re-run all tasks')
+    par.add_argument('-fairness', action="store_true", help='optimize random seed for fairness')
     
-
     return par.parse_args()  	
 
 if __name__ == "__main__":
@@ -24,8 +24,10 @@ if __name__ == "__main__":
         print("input:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}".format(args.input_path, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
         print("mini_tasks:{}\nreset_tasks:{}".format(args.mini_tasks, args.reset_tasks))        
         run_tasks(args.input_path, args.dataset+".txt", args.feats_path, args.feature_type, 
-        args.output_path, args.cache_path, args.reset_tasks, args.tune, None, args.mini_tasks)    
+        args.output_path, args.cache_path, args.reset_tasks, args.tune, None, args.mini_tasks,
+        fairness=args.fairness)    
     else:        
         print("input:{}\ndataset:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}\n".format(args.input_path, args.dataset, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
-        run_analyses(args.input_path, args.dataset, args.feats_path, args.feature_type, args.output_path, 
-                    args.cache_path, args.clear_results, args.tune)
+        run_analyses(args.input_path, args.dataset, args.feats_path, args.feature_type, 
+                    args.output_path, args.cache_path, args.clear_results, args.tune,
+                    fairness=args.fairness)

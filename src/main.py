8f86f19208bfc05870e126a39f0ce300e39195d0
Silvio Moreira <silvio.moreira@qntfy.com>
2020-11-05 16:59:27 -0800

    fixed bug due to renaming

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import argparse
-from MIMIC_Outcomes import run_analyses, run_tasks
+from MIMIC_Outcomes import run_analyses, run_tasks, extract_features
 
 def get_args():
     par = argparse.ArgumentParser(description="MIMIC runs")
@@ -14,20 +14,25 @@ def get_args():
     par.add_argument('-mini_tasks', action="store_true", help='use small datasets')
     par.add_argument('-reset_tasks', action="store_true", help='re-run all tasks')
     par.add_argument('-fairness', action="store_true", help='optimize random seed for fairness')
+    par.add_argument('-feature_extraction', action="store_true", help='extract features')
     
     return par.parse_args()  	
 
 if __name__ == "__main__":
     args = get_args()    
-    if "tasks" in args.dataset:
-        print("[running tasks: {}]".format(args.dataset))
-        print("input:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}".format(args.input_path, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
-        print("mini_tasks:{}\nreset_tasks:{}".format(args.mini_tasks, args.reset_tasks))        
-        run_tasks(args.input_path, args.dataset+".txt", args.feats_path, args.feature_type, 
-        args.output_path, args.cache_path, args.reset_tasks, args.tune, None, args.mini_tasks,
-        fairness=args.fairness)    
-    else:        
-        print("input:{}\ndataset:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}\n".format(args.input_path, args.dataset, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
-        run_analyses(args.input_path, args.dataset, args.feats_path, args.feature_type, 
-                    args.output_path, args.cache_path, args.clear_results, args.tune,
-                    fairness=args.fairness)
+    if args.feature_extraction:
+        print("[extracting features: {} @ {}]".format(args.feature_type, args.feats_path))
+        extract_features(feature_type=args.feature_type, path=args.feats_path)
+    else:
+        if "tasks" in args.dataset:
+            print("[running tasks: {}]".format(args.dataset))
+            print("input:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}".format(args.input_path, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
+            print("mini_tasks:{}\nreset_tasks:{}".format(args.mini_tasks, args.reset_tasks))        
+            run_tasks(args.input_path, args.dataset+".txt", args.feats_path, args.feature_type, 
+            args.output_path, args.cache_path, args.reset_tasks, args.tune, None, args.mini_tasks,
+            fairness=args.fairness)    
+        else:        
+            print("input:{}\ndataset:{}\nfeature_type:{}\noutput:{}\ncache:{}\nclear_results:{}\ntune:{}\n".format(args.input_path, args.dataset, args.feature_type, args.output_path, args.cache_path, args.clear_results, args.tune))
+            run_analyses(args.input_path, args.dataset, args.feats_path, args.feature_type, 
+                        args.output_path, args.cache_path, args.clear_results, args.tune,
+                        fairness=args.fairness)

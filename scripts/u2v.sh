e08b3abc547b07eb1d49954dd6b131f760a8d758
Silvio Moreira <silvio.moreira@qntfy.com>
2021-01-31 16:16:21 -0800

    updated u2v

--- scripts/u2v.sh
+++ scripts/u2v.sh
@@ -2,19 +2,19 @@ U2V_PATH="/Users/samir/Dev/projects/user2vec_torch/user2vec"
 BASE_PATH="/Users/samir/Dev/projects/MIMIC/MIMIC"
 WORD_EMBEDDINGS="/Users/samir/Dev/resources/embeddings/word_embeddings.txt"
 
-# U2V_PATH="/Users/samir/Dev/projects/user2vec_torch/user2vec"
-# BASE_PATH="/media/data_1/samir/projects/MIMIC"
-# WORD_EMBEDDINGS="/Users/samir/Dev/resources/embeddings/word_embeddings.txt"
+U2V_PATH="/home/silvio/home/projects/user2vec_torch/user2vec/"
+BASE_PATH="/media/data_1/samir/projects/MIMIC"
+WORD_EMBEDDINGS="/media/data_1/samir/resources/word_embeddings.txt"
 
 INPUT=$BASE_PATH"/DATA/input/patients.csv"
 OUTPUT_PATH=$BASE_PATH"/DATA/features/u2v/"
 
 python src/get_u2v_data.py -input_file $INPUT -output_folder $OUTPUT_PATH 
-
-# python $U2V_PATH/build.py -input $CORPUS -emb $WORD_EMBEDDINGS -output $PKL_PATH
-
-# python $U2V_PATH/train.py -input $PKL_PATH  -output $OUTPUT_PATH
-                   
-                   
-                    
-
+CORPUS=$OUTPUT_PATH"/u2v/users.txt"
+python $U2V_PATH"run.py" -input $CORPUS -emb $WORD_EMBEDDINGS -output $OUTPUT_PATH \
+                        -lr 0.001 \
+                        -epochs 20 \
+                        -neg_samples 2 \
+                        -margin 5 \
+			-device cpu \
+			-train
